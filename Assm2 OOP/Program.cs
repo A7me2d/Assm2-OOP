fc446d946931f6a3e524b40519f170e27ad2d4a7
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-09 20:47:42 +0300

    Vid 1

--- Assm2 OOP/Program.cs
+++ Assm2 OOP/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Assm2_OOP
+﻿using Assm2_OOP.Encapsulation;
+
+namespace Assm2_OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region ENCapsu
+            Employee employee = new Employee( 10 , "Ahmed" , 3000);
+            Console.WriteLine(employee);
+
+            ////employee.Id = 1;
+            ////Console.WriteLine(employee.Id);
+
+            //employee.SetName("Hany Makram"); //set
+            //Console.WriteLine(employee.GetName()); //Get
+
+
+            //employee.Salary = 1000;
+            //Console.WriteLine(employee.Salary);
+
+            employee.Salary = 2000;
+            //Console.WriteLine(employee.Salary);
+
+            employee.Age = 25;
+            //Console.WriteLine(employee.Age);
+
+            Console.WriteLine(employee.Deducation);
+            #endregion
+
+
         }
     }
 }
