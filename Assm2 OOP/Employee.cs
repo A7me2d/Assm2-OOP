dac79dc2fa7e209797654fa7be796b2a41be19a8
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-11 17:41:43 +0300

    Assiment 02

---
+++ Assm2 OOP/Employee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assm2_OOP
+{
+    public enum SecurityPrivilege
+    {
+        Guest,
+        Developer,
+        Secretary,
+        DBA
+    }
+
+    public class HiringDate
+    {
+        public int Day { get; private set; }
+        public int Month { get; private set; }
+        public int Year { get; private set; }
+
+        public HiringDate(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day}/{Month}/{Year}";
+        }
+    }
+
+    public class Employee
+    {
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public SecurityPrivilege SecurityLevel { get; private set; }
+        public decimal Salary { get; private set; }
+        public HiringDate HireDate { get; private set; }
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    throw new ArgumentException("Gender must be 'M' or 'F'.");
+            }
+        }
+
+        public Employee(int id, string name, SecurityPrivilege securityLevel, decimal salary, HiringDate hireDate, char gender)
+        {
+            ID = id;
+            Name = name;
+            SecurityLevel = securityLevel;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("ID: {0}, Name: {1}, Security Level: {2}, Salary: {3:C}, Hire Date: {4}, Gender: {5}",
+                                 ID, Name, SecurityLevel, Salary, HireDate, Gender);
+        }
+    }
+}
--- Assm2 OOP/Program.cs
+++ Assm2 OOP/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 :: Define a struct "Person" with properties "Name" and "Age"
+            //Person[] people = new Person[3];
+
+            //people[0] = new Person("Ahmed", 25);
+            //people[1] = new Person("Hany", 30);
+            //people[2] = new Person("Makram", 40);
+
+            //foreach (Person person in people)
+            //{
+            //    Console.WriteLine(person);
+            //}
+            #endregion
+
+            #region Q2 ::  the oldest person
+
+            //Person[] people = new Person[3];
+
+            //for (int i = 0; i < people.Length; i++) { 
+            //Console.WriteLine($"Enter Name For Person number {i+1}");
+            //   string name =  Console.ReadLine();
+            //Console.WriteLine($"Enter Age Person Number {i+1}");
+            //    int age = int.Parse(Console.ReadLine());
+            //    people[i] = new Person(name, age);
+            //}
+
+            //Person oldperson = people[0];
+
+            //for (int i = 1 ; i < people.Length; i++)
+            //{
+            //    if (people[i].Age > oldperson.Age) { 
+            //    oldperson = people[i];
+            //    }
+            //}
+
+            //Console.WriteLine("The Oldest person is : ");
+            //Console.WriteLine(oldperson);
+            #endregion
+
+            #region Q3 Part03 :: Design and implement a Class for the employees
+
+            //Employee[] EmpArr = new Employee[3];
+
+            //HiringDate hireDate1 = new HiringDate(11, 7, 2024);
+            //HiringDate hireDate2 = new HiringDate(12, 8, 2024);
+            //HiringDate hireDate3 = new HiringDate(13, 9, 2024);
+
+            //EmpArr[0] = new Employee(1, "Ahmed Hany", SecurityPrivilege.DBA, 7500, hireDate1, 'M');
+            //EmpArr[1] = new Employee(2, "Non Non", SecurityPrivilege.Guest, 4000, hireDate2, 'F');
+            //EmpArr[2] = new Employee(3, "Omer Matter", SecurityPrivilege.Developer, 6000, hireDate3, 'M');
+
+            //foreach (Employee emp in EmpArr)
+            //{
+            //    Console.WriteLine(emp);
+            //}
+            #endregion
+
         }
     }
 }
